d47aa79d8aa5d8327881143cf221a68b838e4a0d
Aron Lee Freyer <aron-freyer@gmx.de>
2023-03-20 21:55:40 +0100

    Extend data augmentation and remove incomplete sets of data

--- data_plot/dataPreprocessor/dataAugmentation.py
+++ data_plot/dataPreprocessor/dataAugmentation.py
@@ -6,36 +6,56 @@ import sys
 class DataAugmentator(DataPreprocessor):
     invalid_val = -99
 
-    def __init__(self, filter_factor: int, sensor_count: int) -> None:
+    def __init__(self, filter_factor: int, sensor_count: int, max_samples: int = None) -> None:
         self.filter_factor: int = filter_factor
         self.sensor_data: list[pd.DataFrame] = []
         self.sensor_count = sensor_count
+        self.max_samples = max_samples
 
-    def _removeIncompleteSet(self) -> None:
-        pass
+    def _filterIncompleteSets(self, raw_data) -> pd.DataFrame:
+        expected_numb = 0
+
+        # remove very first elements if sensor 0 doesn't start
+        while(int(raw_data.loc[[0]]["sensor_id"]) != 0):
+            raw_data.drop(index=0, inplace=True)
+            raw_data.reset_index(drop=True, inplace=True)
+
+        # remove gaps in between
+        for index, row in enumerate(raw_data.iterrows(), 1):
+            if int(row[1].sensor_id) == expected_numb:
+                expected_numb = (expected_numb + 1) % self.sensor_count
+            else:
+                found = False
+                i = 1
+                while(not found):
+                    if int(raw_data.loc[[index-i]]["sensor_id"]) == 0:
+                        found = True
+                    raw_data.drop(index=index-i, inplace=True)
+                    i += 1
+
+        # make sure that last set is complete
+        while(int(raw_data.loc[[raw_data.last_valid_index()]]["sensor_id"]) != (self.sensor_count - 1)):
+            raw_data.drop(index=raw_data.last_valid_index(), inplace=True)
+            raw_data.reset_index(drop=True, inplace=True)
+
+        return raw_data
 
     def preprocessData(self, raw_data: pd.DataFrame) -> None:
         if self.sensor_data == []:
             raw_data = raw_data.replace(DataAugmentator.invalid_val, np.nan)
 
-            expected_numb = 0
-            for index, row in enumerate(raw_data.iterrows(), 1):
-                if int(row[1].sensor_id) == expected_numb:
-                    expected_numb = (expected_numb + 1) % 3
-                else:
-                    found = False
-                    i = 1
-                    while(not found):
-                        if int(raw_data.loc[[index-i]]["sensor_id"]) == 0:
-                            found = True
-                        raw_data.drop(index=index-i, inplace=True)
-                        i += 1
+            if self.max_samples is not None:
+                raw_data = raw_data.tail(self.max_samples * self.sensor_count + 1)
+                raw_data.reset_index(inplace=True)
+
+            raw_data = self._filterIncompleteSets(raw_data)
 
             for sensor_id in range(self.sensor_count):
                 data = raw_data[raw_data["sensor_id"] == sensor_id]
                 data = data.iloc[::self.filter_factor]
                 data = data.fillna(method='ffill')
                 self.sensor_data.append(data)
+
         else:
             sys.exit(-1)
 
--- data_plot/plot_weather_data_mod.py
+++ data_plot/plot_weather_data_mod.py
@@ -4,60 +4,45 @@ from dataReader.csvReader import csvReader
 from dataPreprocessor.dataAugmentation import DataAugmentator
 
 data_path = "..\\data\\Test02\\"
-nth = 100
-lastX = 14400
+nth = 2
 
 csv_reader = csvReader(data_path)
 df = csv_reader.getRawData()
 
-preprocessor = DataAugmentator(nth, 3)
+preprocessor = DataAugmentator(nth, sensor_count=3, max_samples=None)
 preprocessor.preprocessData(df)
 
 df_outdoor = preprocessor.getDataSensorID(0)
+df_indoor = preprocessor.getDataSensorID(2)
+
 df_dateOnly = pd.to_datetime(df_outdoor["time"]).dt.date
 dayChangeList_outdoor = df_dateOnly[df_dateOnly.diff() != "0 days"].index.tolist()
 
-df_indoor = preprocessor.getDataSensorID(2)
-df_dateOnly = pd.to_datetime(df_indoor["time"]).dt.date
-dayChangeList_indoor = df_dateOnly[df_dateOnly.diff() != "0 days"].index.tolist()
-
-
-fig1, axs = plt.subplots(2)
-fig1.suptitle('Temperature')
-axs[0].plot(df_outdoor["time"], df_outdoor["temp"], marker=".")
-axs[0].plot(df_indoor["time"], df_indoor["temp"], marker=".")
-axs[0].set_title("Outdoor Temperature")
+plt.figure("Temperature")
+plt.plot(df_outdoor["time"], df_outdoor["temp"], marker=".")
+plt.plot(df_indoor["time"], df_indoor["temp"], marker=".")
+plt.title("Temperature")
 for day in dayChangeList_outdoor[1:]:
-    axs[0].axvline(x = df_outdoor["time"].loc[day], color = 'k', label = 'axvline - full height')
+    plt.axvline(x = df_outdoor["time"].loc[day], color = 'k', label = 'axvline - full height')
 
-for ax in axs:
-    plt.sca(ax)
-    plt.gca().xaxis.set_major_locator(plt.MaxNLocator(5))
-    plt.gca().axes.set_xlabel("Date (CET)")
-    plt.gca().axes.set_ylabel("Temprature in °C")
-    plt.gca().axes.set_ylim(-5,25)
-
+plt.gca().xaxis.set_major_locator(plt.MaxNLocator(5))
+plt.gca().axes.set_xlabel("Date (CET)")
+plt.gca().axes.set_ylabel("Temprature in °C")
+plt.gca().axes.set_ylim(-5,25)
 
 
 ########################################################################
 
-fig2, axs = plt.subplots(2)
-axs[0].plot(df_outdoor["time"], df_outdoor["humidity"], marker=".")
-axs[0].set_title("Outdoor humidity")
+plt.figure("Humidity")
+plt.plot(df_outdoor["time"], df_outdoor["humidity"], marker=".")
+plt.plot(df_indoor["time"], df_indoor["humidity"], marker=".")
+plt.title("Humidity")
 for day in dayChangeList_outdoor[1:]:
-    axs[0].axvline(x = df_outdoor["time"].loc[day], color = 'k', label = 'axvline - full height')
+    plt.axvline(x = df_outdoor["time"].loc[day], color = 'k', label = 'axvline - full height')
 
-axs[1].plot(df_indoor["time"], df_indoor["humidity"], marker=".")
-axs[1].set_title("Indoor humidity")
-for day in dayChangeList_indoor[1:]:
-    axs[1].axvline(x = df_indoor["time"].loc[day], color = 'k', label = 'axvline - full height')
-
-
-for ax in axs:
-    plt.sca(ax)
-    plt.gca().xaxis.set_major_locator(plt.MaxNLocator(5))
-    plt.gca().axes.set_xlabel("Date (CET)")
-    plt.gca().axes.set_ylabel("Humidity in %")
-    plt.gca().axes.set_ylim(0,100)
+plt.gca().xaxis.set_major_locator(plt.MaxNLocator(5))
+plt.gca().axes.set_xlabel("Date (CET)")
+plt.gca().axes.set_ylabel("Humidity in %")
+plt.gca().axes.set_ylim(-5,105)
 
 plt.show()
