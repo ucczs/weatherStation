5d4c78291bd9d55f8e37bcdbb1ac1308fbd0429b
Aron Lee Freyer <aron-freyer@gmx.de>
2023-04-23 11:21:23 +0200

    Create subplot for temperature

--- data_plot/plot_weather_data.py
+++ data_plot/plot_weather_data.py
@@ -18,23 +18,59 @@ mhz19_co2 = preprocessor.getDataSensorID(2)
 api_temp = preprocessor.getDataSensorID(3)
 api_humidity = preprocessor.getDataSensorID(4)
 
+
+########################################################################
+###### Temperature
+
 df_dateOnly = pd.to_datetime(dht20_temp["time"]).dt.date
 dayChangeList = df_dateOnly[df_dateOnly.diff() != "0 days"].index.tolist()
+subplot = True
 
-plt.figure("Temperature")
-plt.plot(dht20_temp["time"], dht20_temp["sensor_value"], marker=".")
-plt.plot(api_temp["time"], api_temp["sensor_value"], marker=".")
-plt.title("Temperature")
-for day in dayChangeList[1:]:
-    plt.axvline(x = dht20_temp["time"].loc[day], color = 'k', label = 'axvline - full height')
+if subplot:
+    numbOfSubplots = 2
+    fig, axs = plt.subplots(numbOfSubplots)
 
-plt.gca().xaxis.set_major_locator(plt.MaxNLocator(5))
-plt.gca().axes.set_xlabel("Date (CET)")
-plt.gca().axes.set_ylabel("Temprature in °C")
-plt.gca().axes.set_ylim(0,25)
+    axs[0].set_title("In- and outside temperature")
+    axs[0].plot(dht20_temp["time"], dht20_temp["sensor_value"], marker=".")
+    axs[0].plot(api_temp["time"], api_temp["sensor_value"], marker=".")
+
+    axs[1].set_title("Inside temperature")
+    axs[1].plot(dht20_temp["time"], dht20_temp["sensor_value"], marker=".")
+
+    for i in range(numbOfSubplots):
+        axs[i].xaxis.set_major_locator(plt.MaxNLocator(5))
+        axs[i].axes.set_xlabel("Date (CET)")
+        axs[i].axes.set_ylabel("Temprature in °C")
+        for day in dayChangeList[1:]:
+            axs[i].axvline(x = dht20_temp["time"].loc[day], color = 'k', label = 'axvline - full height')
+
+    plt.subplots_adjust(left=0.1,
+                        bottom=0.1,
+                        right=0.9,
+                        top=0.9,
+                        wspace=0.4,
+                        hspace=0.4)
+
+    plt.gcf().canvas.manager.set_window_title("Temperature")
+
+else:
+    plt.figure("Temperature")
+    plt.plot(dht20_temp["time"], dht20_temp["sensor_value"], marker=".")
+    plt.plot(api_temp["time"], api_temp["sensor_value"], marker=".")
+    plt.title("Temperature")
+    for day in dayChangeList[1:]:
+        plt.axvline(x = dht20_temp["time"].loc[day], color = 'k', label = 'axvline - full height')
+
+    plt.gca().xaxis.set_major_locator(plt.MaxNLocator(5))
+    plt.gca().axes.set_xlabel("Date (CET)")
+    plt.gca().axes.set_ylabel("Temprature in °C")
+    plt.gca().axes.set_ylim(0,25)
+
+
 
 
 ########################################################################
+###### Humidity
 
 df_dateOnly = pd.to_datetime(dht20_humidity["time"]).dt.date
 dayChangeList = df_dateOnly[df_dateOnly.diff() != "0 days"].index.tolist()
@@ -52,6 +88,7 @@ plt.gca().axes.set_ylabel("Humidity in %")
 plt.gca().axes.set_ylim(10,100)
 
 ########################################################################
+###### CO2
 
 df_dateOnly = pd.to_datetime(mhz19_co2["time"]).dt.date
 dayChangeList = df_dateOnly[df_dateOnly.diff() != "0 days"].index.tolist()
